be4312c67262d667e67ebbe115711f9630a15d32
mpixton <71086131+mpixton@users.noreply.github.com>
2021-02-11 22:44:12 -0700

    DATA: added Book model
    Added the Book model with the properties ISBN, Title, and Price. Added the Required attribute on all properties. Added RegEx to enforce proper ISBN format. Added doc for the class and members.

---
+++ Models/Book.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AmazonStartUp.Models
+{
+    /// <summary>
+    /// Represents a Book. Author, Publisher, Category, and Sub-Category are all related to Book, but stored in their own model.
+    /// </summary>
+    public class Book
+    {
+        /// <summary>
+        /// International Standard Book Number of the Book. Primary key of the model's table.
+        /// </summary>
+        [Required]
+        [RegularExpression(@"^[0-9]{3}-[0-9]{10}$",
+            ErrorMessage = "Format: ###-##########")]
+        public string ISBN { get; set; }
+        
+        /// <summary>
+        /// Title of the Book. 
+        /// </summary>
+        [Required]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Price of the Book.
+        /// </summary>
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
