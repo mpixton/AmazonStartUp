416bce523281f5b72fc6886ac21735b67c9d28d7
mpixton <71086131+mpixton@users.noreply.github.com>
2021-02-20 12:02:18 -0700

    FEAT: add BookViewModel
    Added a BookViewModel class so that pagination can be used dynamically.

---
+++ Models/ViewModels/BookViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AmazonStartUp.Models.ViewModels
+{
+    public class BookViewModel
+    {
+        public IEnumerable<Book> Books { get; set; }
+
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
