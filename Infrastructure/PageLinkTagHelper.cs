d527b89b423b76a711f639e3bda3fdc0d04ad6f6
mpixton <71086131+mpixton@users.noreply.github.com>
2021-02-20 12:01:23 -0700

    FEAT: pagination tag helper
    Added a pagination tag helper class to the project.

---
+++ Infrastructure/PageLinkTagHelper.cs
@@ -0,0 +1,53 @@
+﻿using AmazonStartUp.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AmazonStartUp.Infrastructure
+{
+    [HtmlTargetElement("div", Attributes = "page-model")]
+    public class PageLinkTagHelper : TagHelper
+    {
+        private IUrlHelperFactory _urlHelperFactory;
+
+        public PageLinkTagHelper(IUrlHelperFactory urlhf)
+        {
+            _urlHelperFactory = urlhf;
+        }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+
+        public PagingInfo PageModel { get; set; }
+
+        public string PageAction { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            IUrlHelper urlHelper = _urlHelperFactory.GetUrlHelper(ViewContext);
+
+            var result = new TagBuilder("div");
+
+            // Loop through the total number of pages in the PageModel class.
+            for (int i = 1; i <= PageModel.TotalPages; i++)
+            {
+                // For each page, create a new a tag with the URL after the pattern /?page=i.
+                var tag = new TagBuilder("a");
+                tag.Attributes["href"] = urlHelper.Action(PageAction, new { page = i });
+                tag.InnerHtml.Append(i.ToString());
+
+                // Add the newly created a tag to the output.
+                result.InnerHtml.AppendHtml(tag);
+            }
+
+            output.Content.AppendHtml(result.InnerHtml);
+        }
+    }
+}
