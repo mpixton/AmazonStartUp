7af2167f147ddfc0653d930c3aae8bf254fd5bb5
mpixton <71086131+mpixton@users.noreply.github.com>
2021-02-20 13:11:02 -0700

    FEAT: add css to pagination tag helper
    Added optional CSS classes to apply to the pagination links.

--- Infrastructure/PageLinkTagHelper.cs
+++ Infrastructure/PageLinkTagHelper.cs
@@ -21,14 +21,48 @@ namespace AmazonStartUp.Infrastructure
             _urlHelperFactory = urlhf;
         }
 
+        /// <summary>
+        /// Context of the view around the Tag Helper.
+        /// </summary>
         [ViewContext]
         [HtmlAttributeNotBound]
         public ViewContext ViewContext { get; set; }
 
+        /// <summary>
+        /// Object containing the Pagination information.
+        /// </summary>
         public PagingInfo PageModel { get; set; }
 
+        /// <summary>
+        /// Action for the Tag to execute.
+        /// </summary>
         public string PageAction { get; set; }
 
+        /// <summary>
+        /// Determines if the Tag Helper will use CSS to display each link.
+        /// </summary>
+        public bool PageClassesEnabled { get; set; } = false;
+
+        /// <summary>
+        /// CSS class to be applied to the Page control.
+        /// </summary>
+        public string PageClass { get; set; }
+
+        /// <summary>
+        /// CSS class to be applied to the links that are not selected.
+        /// </summary>
+        public string PageClassNormal { get; set; }
+
+        /// <summary>
+        /// CSS class to be applied to the link that is selected.
+        /// </summary>
+        public string PageClassSelected { get; set; }
+
+        /// <summary>
+        /// Processes the Tag Helper when the page is called.
+        /// </summary>
+        /// <param name="context">Context around the tag in the View.</param>
+        /// <param name="output">HTML elements to be output.</param>
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             IUrlHelper urlHelper = _urlHelperFactory.GetUrlHelper(ViewContext);
@@ -41,6 +75,14 @@ namespace AmazonStartUp.Infrastructure
                 // For each page, create a new a tag with the URL after the pattern /?page=i.
                 var tag = new TagBuilder("a");
                 tag.Attributes["href"] = urlHelper.Action(PageAction, new { page = i });
+
+                // Checks if we have enabled extra CSS classes for our Tag Helper.
+                if (PageClassesEnabled)
+                {
+                    tag.AddCssClass(PageClass);
+                    tag.AddCssClass(i == PageModel.CurrentPage ? PageClassSelected : PageClassNormal);
+                }
+
                 tag.InnerHtml.Append(i.ToString());
 
                 // Add the newly created a tag to the output.
