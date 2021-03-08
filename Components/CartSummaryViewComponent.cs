e67a6a34d9695f529ea49719514c7b585defe3ad
mpixton <71086131+mpixton@users.noreply.github.com>
2021-03-08 09:58:03 -0700

    FEAT: cart summary in navbar
    Add a View Component for the Cart summary. Displayed in the navbar under the categories, but above the pagination buttons.

---
+++ Components/CartSummaryViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AmazonStartUp.Models;
+using AmazonStartUp.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AmazonStartUp.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Cart _Cart;
+
+        public CartSummaryViewComponent(Cart cartService)
+        {
+            _Cart = cartService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(_Cart);
+        }
+    }
+}
